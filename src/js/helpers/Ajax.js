4d8630dacfe0a3105195726cc12b0458b9ab3e6e
DZakh <dzakh.dev@gmail.com>
2020-01-02 21:13:04 +0300

    Updating Ajax helper

--- src/js/helpers/Ajax.js
+++ src/js/helpers/Ajax.js
@@ -1,68 +1,71 @@
-export default class Ajax {
-  constructor(url) {
-    this.url = url;
-  }
-
-  async get(headers = {}) {
+export const Ajax = {
+  get: async (url, headers = {}) => {
     try {
-      const res = await fetch(this.url, {
+      const res = await fetch(url, {
         method: 'GET',
         headers
       });
       const { status } = res;
+
       if (status !== 200) {
         return { status };
       }
+
       const data = await res.json();
+
       return { status, ...data };
     } catch (err) {
       throw new Error(err);
     }
-  }
+  },
 
-  async post(data = {}) {
+  post: async (url, data = {}) => {
     try {
-      const res = await fetch(this.url, {
+      const res = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(data),
         headers: { 'Content-Type': 'application/json' }
       });
+
       return res.json();
     } catch (err) {
       throw new Error(err);
     }
-  }
+  },
 
-  async getById(id) {
+  getById: async (url, id) => {
     try {
-      const res = await fetch(this.url + '/' + id);
+      const res = await fetch(`${url}/${id}`);
+
       return res.json();
     } catch (err) {
       throw new Error(err);
     }
-  }
+  },
 
-  async deleteById(id) {
+  deleteById: async (url, id) => {
     try {
-      const res = await fetch(this.url + '/' + id, {
+      const res = await fetch(`${url}/${id}`, {
         method: 'DELETE'
       });
+
       return res.json();
     } catch (err) {
       throw new Error(err);
     }
-  }
+  },
 
-  async updateById(id, data = {}) {
+  updateById: async (url, id, data = {}) => {
     try {
-      const res = await fetch(this.url + '/' + id, {
+      const res = await fetch(`${url}/${id}`, {
         method: 'PATCH',
         body: JSON.stringify(data),
         headers: { 'Content-Type': 'application/json' }
       });
+
       return res.json();
     } catch (err) {
       throw new Error(err);
     }
   }
-}
+};
