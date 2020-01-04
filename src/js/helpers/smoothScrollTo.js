97a55426904cb3e2da4f4c08f5af407d5765973d
DZakh <dzakh.dev@gmail.com>
2020-01-04 14:33:46 +0300

    Updating smoothScrollTo function

--- src/js/helpers/smoothScrollTo.js
+++ src/js/helpers/smoothScrollTo.js
@@ -1,18 +1,24 @@
 const smoothScrollTo = elementId => {
-  const element = document.getElementById(elementId);
-  element.scrollIntoView({
+  if (!elementId) {
+    throw new Error(
+      'Dataset not found. If you have one, then you need to add `pointer-event:none` to elements inside of your button.'
+    );
+  }
+
+  const e = document.getElementById(elementId);
+
+  e.scrollIntoView({
     block: 'start',
     behavior: 'smooth'
   });
 };
 
 export const activateSmoothScroll = () => {
-  const links = document.querySelectorAll('.smooth-scroll-to');
+  const links = document.querySelectorAll('[data-smooth-scroll-to]');
+
   links.forEach(link => {
-    link.addEventListener('click', element => {
-      element.preventDefault();
-      element.stopPropagation();
-      smoothScrollTo(element.target.dataset.id);
+    link.addEventListener('click', e => {
+      smoothScrollTo(e.target.dataset.smoothScrollTo);
     });
   });
 };
